de46a05eaeb773d0430537164565200e646c514b
Said-Nuri <datron1111@gmail.com>
2015-12-07 22:03:06 +0200

    Looping mq tests added

--- mq_test/receiver.c
+++ mq_test/receiver.c
@@ -21,62 +21,80 @@
 #include "test_params.h" /* NUM_OF_MSG, SIZE_OF_MSG */
 
 /* name of the POSIX object referencing the queue */
-#define MSGQOBJ_NAME    "/myqueue1"
+#define MSGQOBJ_NAME    "/myqueue"
 /* max length of a message (just for this process) */
 #define MAX_MSG_LEN 10000
 #define BILLION 1E9
 #define PERMS O_RDONLY
+#define MQ_PRIO 10
+
+int open_message_queue();
 
 int main(int argc, char *argv[]) {
-    int i;
-    int msgsz;
-    unsigned int sender;
+    int i, j, k;
     double accum;
-    char msgcontent[MAX_MSG_LEN];
-    struct mq_attr msgq_attr = {0};
-    struct timespec requestStart, requestEnd;
     mqd_t msgq_id;
+    char msgcontent[MAX_MSG_LEN];
+    struct timespec requestStart, requestEnd;
+    int MSG_COUNT_PER_TEST = (int)sizeof(NUM_OF_MSG)/sizeof(int);
+    int MSG_SIZE_PER_TEST = (int)sizeof(SIZE_OF_MSG)/sizeof(int);
+
+    for (k = 0; k < MSG_SIZE_PER_TEST; ++k){
+
+    	msgq_id = open_message_queue(k);
+
+    	mq_receive(msgq_id, msgcontent, SIZE_OF_MSG[k]+1, NULL);
+    	mq_send(msgq_id, msgcontent, SIZE_OF_MSG[k], MQ_PRIO);
+
+	    for (j = 0; j < MSG_COUNT_PER_TEST; ++j){
+	    	printf("**********************************************************************\n");
+	    	printf("Test %d is starting... [Number of messages:%d] [Size of a message:%d]\n", j+1, NUM_OF_MSG[j], SIZE_OF_MSG[k]);
+	    	printf("msgq_id: %d\n", msgq_id);
+		    clock_gettime(CLOCK_REALTIME, &requestStart);
+
+		    for (i = 0; i < NUM_OF_MSG[j]; ++i){
+			    mq_receive(msgq_id, msgcontent, SIZE_OF_MSG[k]+1, NULL);
+		    }
+
+		    // Get the ending time 
+		    clock_gettime(CLOCK_REALTIME, &requestEnd);
+
+		    accum = ( requestEnd.tv_sec - requestStart.tv_sec ) 
+		    + ( requestEnd.tv_nsec - requestStart.tv_nsec ) / BILLION;
+
+		    printf("Test %d is finished.\n", j+1);
+		    printf( "Elapsed time: %.8lf sec. \n", accum );
+		    printf("Avarage time: %.8lf sec. \n", accum/NUM_OF_MSG[j]);
+		    printf("**********************************************************************\n\n\n");
+		    sleep(1);
+		}
+
+		/* closing the queue    --  mq_close() */
+    	//mq_close(msgq_id);
+
+    	sleep(3);
+	}
     
-    /* opening the queue        --  mq_open() */
-    msgq_id = mq_open(MSGQOBJ_NAME, PERMS);
+    return 0;
+}
+int open_message_queue(int index){
+	mqd_t msgq_id;
+	struct mq_attr msgq_attr = {0};
+	char MQNAME[50] = {0};
+
+	sprintf(MQNAME, "%s%d", MSGQOBJ_NAME, index);
+	printf("mq_name : %s\n", MQNAME);
+
+	/* opening the queue        --  mq_open() */
+    msgq_id = mq_open(MQNAME, PERMS);
     
     if (msgq_id == (mqd_t)-1) {
         perror("In mq_open()");
         exit(1);
     }
 
-    /* getting the attributes from the queue        --  mq_getattr() */
     mq_getattr(msgq_id, &msgq_attr);
-    printf("Queue \"%s\":\n\t- stores at most %ld messages\n\t- large at most %ld bytes each\n\t- currently holds %ld messages\n", MSGQOBJ_NAME, msgq_attr.mq_maxmsg, msgq_attr.mq_msgsize, msgq_attr.mq_curmsgs);
+    printf("Queue \"%s\":\n\t- stores at most %ld messages\n\t- large at most %ld bytes each\n\t- currently holds %ld messages\n", MQNAME, msgq_attr.mq_maxmsg, msgq_attr.mq_msgsize, msgq_attr.mq_curmsgs);
 
-    msgsz = mq_receive(msgq_id, msgcontent, SIZE_OF_MSG[0]+1, &sender);
-
-    // Get the starting time 
-    clock_gettime(CLOCK_REALTIME, &requestStart);
-
-    for (i = 0; i < NUM_OF_MSG[0]; ++i){
-    	/* getting a message */
-	    msgsz = mq_receive(msgq_id, msgcontent, SIZE_OF_MSG[0]+1, &sender);
-	    
-	    if (msgsz == -1) {
-	        perror("In mq_receive()");
-	        exit(1);
-	    }	
-    	//printf("Received message (%d bytes) from %d: %s\n", msgsz, sender, msgcontent);
-    }
-
-    // Get the ending time 
-    clock_gettime(CLOCK_REALTIME, &requestEnd);
-
-    accum = ( requestEnd.tv_sec - requestStart.tv_sec ) 
-    + ( requestEnd.tv_nsec - requestStart.tv_nsec ) / BILLION;
-
-    printf( "Elapsed time: %.8lf\n", accum );
-    printf("Avarage time: %.8lf\n", accum/NUM_OF_MSG[0]);
-
-    /* closing the queue    --  mq_close() */
-    mq_close(msgq_id);
-    
-    
-    return 0;
+    return msgq_id;
 }
--- mq_test/sender.c
+++ mq_test/sender.c
@@ -22,36 +22,94 @@
 #include "test_params.h" /* NUM_OF_MSG, SIZE_OF_MSG */
 
 /* name of the POSIX object referencing the queue */
-#define MSGQOBJ_NAME    "/myqueue1"
+#define MSGQOBJ_NAME    "/myqueue"
 /* max length of a message (just for this process) */
 #define MAX_MSG_LEN     64
 #define MQ_PRIO 10
 #define BILLION 1E9
 #define PERMS O_WRONLY | O_CREAT | O_EXCL
 
+/* Opens or creates a new message queue */
+int open_message_queue(int, int);
+
 int main(int argc, char *argv[]) {
     mqd_t msgq_id;
-    pid_t my_pid = getpid();
     char msgcontent[MAX_MSG_LEN] = {0};
     struct timespec requestStart, requestEnd;
     double accum;
-    struct mq_attr msgq_attr = {0};
-    int i;
-    msgq_attr.mq_flags = 0;
-    msgq_attr.mq_maxmsg = 10;
-    msgq_attr.mq_msgsize = SIZE_OF_MSG[0];
+    int i, j, k;
+    int MSG_COUNT_PER_TEST = (int)sizeof(NUM_OF_MSG)/sizeof(int);
+    int MSG_SIZE_PER_TEST = (int)sizeof(SIZE_OF_MSG)/sizeof(int);
+
+
+    //printf("Wait 5 sec... \n");
+    //sleep(5);
+
+    for (k = 0; k < MSG_SIZE_PER_TEST; ++k){
+
+    	msgq_id = open_message_queue(SIZE_OF_MSG[k], k);
+    	
+    	mq_send(msgq_id, msgcontent, SIZE_OF_MSG[k], MQ_PRIO);
+    	mq_receive(msgq_id, msgcontent, SIZE_OF_MSG[k]+1, NULL);
+
+	    for (j = 0; j < MSG_COUNT_PER_TEST; ++j)
+	    {	
+	    	printf("**********************************************************************\n");
+	    	printf("Test %d is starting... [Number of messages:%d] [Size of a message:%d]\n", j+1, NUM_OF_MSG[j], SIZE_OF_MSG[k]);
+	    	printf("msgq_id: %d\n", msgq_id);
+		    clock_gettime(CLOCK_REALTIME, &requestStart);
+		    
+		    for ( i = 0; i < NUM_OF_MSG[j]; ++i){
+		        mq_send(msgq_id, msgcontent, SIZE_OF_MSG[k], MQ_PRIO);
+		    }
+		    
+		    // Get the ending time 
+		    clock_gettime(CLOCK_REALTIME, &requestEnd);
+
+		    accum = ( requestEnd.tv_sec - requestStart.tv_sec ) 
+		    + ( requestEnd.tv_nsec - requestStart.tv_nsec ) / BILLION;
+
+		    printf("Test %d is finished.\n", j+1);
+		    printf( "Elapsed time: %.8lf sec. \n", accum );
+		    printf("Avarage time: %.8lf sec. \n", accum/NUM_OF_MSG[j]);
+		    printf("**********************************************************************\n\n\n");
+		    sleep(1);
+	    }
+	
+	    /* closing the queue */
+	    mq_close(msgq_id);
+
+	    /* remove the queue */
+	    mq_unlink(MSGQOBJ_NAME);
+
+	    sleep(1);
+	}
+    
+    
+    return 0;
+}
+
+int open_message_queue(int msgsize, int index){
+	struct mq_attr msgq_attr = {0};
+	mqd_t msgq_id;
+	char MQNAME[50] = {0};
+
+	msgq_attr.mq_flags = 0;
+    msgq_attr.mq_maxmsg = 10; //?
+    msgq_attr.mq_msgsize = msgsize;
     msgq_attr.mq_curmsgs = 0;
 
-    mq_unlink(MSGQOBJ_NAME);
+    sprintf(MQNAME, "%s%d", MSGQOBJ_NAME, index);
+    printf("mq_name : %s\n", MQNAME);
+
+    mq_unlink(MQNAME);
 
     /* Create Queue */
-    msgq_id = mq_open(MSGQOBJ_NAME, PERMS, S_IRWXU | S_IRWXG, &msgq_attr);
+    msgq_id = mq_open(MQNAME, PERMS, S_IRWXU | S_IRWXG, &msgq_attr);
 
     // If mq exist, try to open
     if( msgq_id == ((mqd_t) -1 ) && (errno == EEXIST) ){
-    	
-    	 /* opening the queue */
-    	msgq_id = mq_open(MSGQOBJ_NAME, O_WRONLY, &msgq_attr);
+    	//msgq_id = mq_open(MQNAME, O_WRONLY, &msgq_attr);
     }
 
     if (msgq_id == (mqd_t)-1) {
@@ -59,38 +117,5 @@ int main(int argc, char *argv[]) {
         exit(1);
     }
 
-    //snprintf(msgcontent, MAX_MSG_LEN, "Hello from process %u ", my_pid);
-
-    printf("Wait 5 sec... ");
-    sleep(5);
-
-    mq_send(msgq_id, msgcontent, SIZE_OF_MSG[0], MQ_PRIO);
-
-    // Get the starting time 
-    clock_gettime(CLOCK_REALTIME, &requestStart);
-    
-    for ( i = 0; i < NUM_OF_MSG[0]; ++i){
-        mq_send(msgq_id, msgcontent, SIZE_OF_MSG[0], MQ_PRIO);
-    }
-    
-    // Get the ending time 
-    clock_gettime(CLOCK_REALTIME, &requestEnd);
-
-    accum = ( requestEnd.tv_sec - requestStart.tv_sec ) 
-    + ( requestEnd.tv_nsec - requestStart.tv_nsec ) / BILLION;
-
-    printf( "Elapsed time: %.8lf\n", accum );
-    printf("Avarage time: %.8lf\n", accum/NUM_OF_MSG[0]);
-
-    /* closing the queue        -- mq_close() */
-    mq_close(msgq_id);
-
-    /*
-    printf("Any key to finish... \n");
-    getchar();
-    */
-
-    mq_unlink(MSGQOBJ_NAME);
-    
-    return 0;
+    return msgq_id;
 }
